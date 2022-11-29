3accb41b341ea1383a771b2a967bd16277cb21a2
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2022-11-29 14:10:37 -0300

    Adding ESLint

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: '@rocketseat/eslint-config/react',
+  rules: {
+    'no-restricted-globals': 1,
+    'no-unused-vars': 1,
+    'prefer-template': 1,
+    'no-empty': 1,
+  },
+}
